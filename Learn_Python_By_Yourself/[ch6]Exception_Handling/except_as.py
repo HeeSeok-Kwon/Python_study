c44c2a0ffe52a270d1f936b7766000ff5ed058bb
HeeSeok-Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-08-08 07:56:32 +0900

    Update except_as.py

--- Learn_Python_By_Yourself/[ch6]Exception_Handling/except_as.py
+++ Learn_Python_By_Yourself/[ch6]Exception_Handling/except_as.py
@@ -4,6 +4,8 @@ try:
   number_input = int(input("정수 입력>"))
   print("{}번째 요소: {}".format(number_input, list_number[number_input]))
 except ValueError as excpetion:
+  print("정수를 입력해주세요!")
   print("excpetion:", excpetion)
 except IndexError as excpetion:
+  print("리스트의 인덱스를 벗어났어요!")
   print("excpetion:", excpetion)
