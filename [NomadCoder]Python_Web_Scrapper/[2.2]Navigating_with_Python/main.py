3d86671255ffea6bd7396166a2757deb4e0cb24d
HeeSeok Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-08-20 10:01:45 +0900

    Create main.py

---
+++ [NomadCoder]Python_Web_Scrapper/[2.2]Navigating_with_Python/main.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50")
+
+print(indeed_result)
+print(indeed_result.text)
+
+# pip install requests
+# pip install beautifulsoup4
