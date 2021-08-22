1dc056f9d7403e87d40e2c352684637ad0ea867e
HeeSeok Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-08-22 10:11:50 +0900

    Create main.py

---
+++ [NomadCoder]Python_Web_Scrapper/[2.4]Extracting_Indeed_Pages_part_Two/main.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+b_tag = []
+for i in range(0, 901, 50):
+    indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&limit=50&start={}".format(i))
+
+    indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+    # print(indeed_soup)
+
+    pagination = indeed_soup.find("div", {"class":"pagination"})
+    # print(pagination)
+
+    pages = pagination.find_all('li')
+    # print(pages)
+    
+    for page in pages:
+        current = page.find("b")
+        if current != None:
+            b_tag.append(int(current.string))
+
+# print(b_tag[-1])
+max_page = b_tag[-1]
+
+# 약 29초 
