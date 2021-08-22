d08302fc60a763de9d616b7d6854f2d18b4fdf65
HeeSeok Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-08-22 11:06:28 +0900

    Update main.py

--- [NomadCoder]Python_Web_Scrapper/[2.3]Extracting_Indeed_Pages/main.py
+++ [NomadCoder]Python_Web_Scrapper/[2.3]Extracting_Indeed_Pages/main.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 b_tag = []
-for i in range(0, 901, 50):
-    indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&limit=50&start={}".format(i))
+for i in range(0, 1001, 50):
+    indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&limit=50&filter=0&start={}".format(i))
 
     indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
     # print(indeed_soup)
