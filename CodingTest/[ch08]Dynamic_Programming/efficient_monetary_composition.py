0dd15e1bad0d76d280825aa090fde29f75c8e6b8
HeeSeok Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-11-01 23:43:26 +0900

    Update efficient_monetary_composition.py

--- CodingTest/[ch08]Dynamic_Programming/efficient_monetary_composition.py
+++ CodingTest/[ch08]Dynamic_Programming/efficient_monetary_composition.py
@@ -8,8 +8,8 @@ d = [10001]*(m+1)
 d[0] = 0
 
 for i in money:
-    for j in range(1, m+1):
-        if j-i >= 0:
+    for j in range(1, m+1): # for j in range(i, m+1): --> loop 횟수 줄일 수 있다.
+        if j-i >= 0:        # Index error만 처리 --> (i-k)원 만드는 방법이 존재하는 경우를 고려한 조건문 필요
             d[j] = min(d[j], d[j-i]+1)
     
 if d[m] == 10001: 
