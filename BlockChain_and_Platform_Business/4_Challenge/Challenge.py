24638e64dc0d0cfcd33f8488431fe0be5feb5220
HeeSeok Kwon <80610295+HeeSeok-Kwon@users.noreply.github.com>
2021-11-06 22:33:36 +0900

    Update Challenge.py

--- BlockChain_and_Platform_Business/4_Challenge/Challenge.py
+++ BlockChain_and_Platform_Business/4_Challenge/Challenge.py
@@ -13,45 +13,47 @@ class Blockchain(object):
     self.chain = []
     # 현재 트랜잭션
     self.current_transactions = []
-    # mine 변수 선언하여 None 할당 (proof_of_work 결과값 담을 변수) 
+    # mine 변수 선언하여 None 할당 (proof_of_work 결과값 담을 변수) -- 0.5점 
     
     # 최초의 블록
     self.new_block(nonce=100, previous_hash=1) # gensis block
   
   # 블록 추가
-  def new_block(self, nonce, previous_hash=None):
-    # 블록 구조 + validation_vlaue 원소 추가
-    # 현재 트랜잭션 비우기
-    # mine 변수 ""로 초기화
-    # 체인에 블록 연결하기
+  def new_block(self, nonce, previous_hash=None): # 3점
+    # 블록 구조 + validation_vlaue 원소 추가 (1점)
+    # 현재 트랜잭션 비우기 (0.5점)
+    # mine 변수 ""로 초기화 (0.5점)
+    # 체인에 블록 연결하기 (1점)
 
     return block
 
   # 트랜잭션 추가
-  def new_transactions(self, sender, recipient, amount):
+  def new_transactions(self, sender, recipient, amount): # 2점
     # 현재 트랜잭션에 추가
-    pass
+    pass # 코드 작성 후 pass 지우기
 
   def hash(self, block):
     block_string = json.dumps(block, sort_keys=True).encode()
     return hashlib.sha256(block_string).hexdigest()
 
   def last_block(self):
-    # 체인의 마지막 원소 반환
-    pass
+    # 체인의 마지막 원소 반환 -- 1점
+    pass # 코드 작성 후 pass 지우기
   
   # 채굴
-  def proof_of_work(self, prev_block):
-    # 난스 값 0으로 할당
-    # prev_block_hash에 prev_block을 hash 함수 취한 값을 할당
-    # valid_proof 호출
+  def proof_of_work(self, prev_block): # 3점(각 0.5점)
+    # 난스 값 0으로 할당 
+    # prev_block_hash에 prev_block을 hash 함수 취한 값을 할당 
+    # valid_proof 호출 (1점)
     # new_transactions 호출 -- 채굴 보상 50코인
     # new_block 호출
     print() # 채굴 후에 출력창 구분하기 위해 추가한 코드
     
 
-  def valid_proof(self, prev_block_hash, current_transactions, nonce):
-    # print() 할 때, 커서 맨 앞으로 위치시켜 출력하기
+  def valid_proof(self, prev_block_hash, current_transactions, nonce): # 3점(각 0.5점)
+    # guess 변수에 prev_block_hash, current_transactions, nonce를 합친 문자열 encode 함수 취하기
+    # guess_hash 변수에 guess를 sha256 함수 취한 값을 할당하기
+    # print() 할 때, 커서 맨 앞으로 위치시켜 출력하기 (1점)
     # mine에 결과값 할당
     # 조건 0을 4개로 변환하기
     pass
@@ -60,12 +62,12 @@ class Blockchain(object):
 bc = Blockchain() # 객체 생성 
 bc.new_transactions('john','smith', 50) # john이 smith에게 50코인 전송
 bc.new_transactions('jenny','sujan', 100) # jenny가 sujan에게 100코인 전송
-# chappie가 simon에게 150코인 전송 코드 추가하기
+# chappie가 simon에게 150코인 전송 코드 추가하기 -- 0.5점
 
-# 현재 체인 상태를 출력하기
+# 현재 체인 상태를 출력하기 -- 0.5점 
 
-# 현재 블록에 저장하기 위해 대기 중인 트랜잭션을 json 형태로 출력하는 코드 추가하기
+# 현재 블록에 저장하기 위해 대기 중인 트랜잭션을 json 형태로 출력하는 코드 추가하기 -- 0.5점
 
-# proof_of_work 호출
+# proof_of_work 호출 -- 0.5점
 
-# 현재 체인 상태를 출력하기
+# 현재 체인 상태를 출력하기 -- 0.5점
